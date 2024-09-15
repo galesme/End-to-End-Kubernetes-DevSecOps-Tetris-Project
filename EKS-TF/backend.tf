ba3f68ebea645deefb8ab2aeee5125386b542591
jackspades <galesme@yahoo.com>
2024-09-15 18:45:20 -0500

    initial

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "terraform-jenkins-pipeline-backend-bucket"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "eks-backend"
     encrypt        = true
   }
   required_version = ">=0.13.0"
